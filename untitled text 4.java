51d5ec50de528e6d99dbfc5ae48d75d355ece9eb
Blingus <43186803+Blingus@users.noreply.github.com>
2018-09-11 18:05:57 -0400

    Add files via upload

---
+++ untitled text 4.java
@@ -0,0 +1,28 @@
+public class PersonInfo1 {
+
+    public static String firstName = "Edgard";
+    public static String lastName = "Sia";
+    public static int studentNo = 7952575;
+    public static String description = "Likes to play soccer";
+
+    public static void displayFr() {
+        String out = SIConstants.GREETING_FR;
+        out += " " + firstName + " " + lastName + '.';
+        out += SIConstants.STUDENT_NUMBER_FR + studentNo + '.';
+        out += SIConstants.DESCRIPTION_FR + description + '.';
+        out += SIConstants.EOL;
+        
+        System.out.println(out);
+    }
+    
+    
+    public static void displayEn() {
+        String out = SIConstants.GREETING_EN;
+        out += " " + firstName + " " + lastName + '.';
+        out += SIConstants.STUDENT_NUMBER_EN + studentNo + '.';
+        out += SIConstants.DESCRIPTION_EN + description + '.';
+        out += SIConstants.EOL;
+        
+        System.out.println(out);
+    }
+}
